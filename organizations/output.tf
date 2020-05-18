4b4994e5bbf5e28e4544f19db2a4925ebfdf1d33
luktom <luktom@luktom.net>
2020-05-18 12:55:11 +0200

    Add output for accounts

--- organizations/output.tf
+++ organizations/output.tf
@@ -1,3 +1,7 @@
 output "aws_organization_arn" {
-  value = "${aws_organizations_organization.main.arn}"
+  value = aws_organizations_organization.main.arn
+}
+
+output "accounts" {
+  value = {for a in aws_organizations_account.account: a.name => a.id}
 }
