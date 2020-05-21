8a72be286ff809608e904f3f3729dd33437623d6
luktom <luktom@luktom.net>
2020-05-21 14:44:34 +0200

    Add modules

--- kafka/output.tf
+++ kafka/output.tf
@@ -1,11 +1,11 @@
-output "zookeeper_connect_string" {
-  value = aws_msk_cluster.kafka.zookeeper_connect_string
+output "zookeeper_connection_string" {
+  value = compact(split(",", aws_msk_cluster.kafka.zookeeper_connect_string))
 }
 
 output "bootstrap_brokers" {
-  value = aws_msk_cluster.kafka.bootstrap_brokers
+  value = compact(split(",", aws_msk_cluster.kafka.bootstrap_brokers))
 }
 
 output "bootstrap_brokers_tls" {
-  value = aws_msk_cluster.kafka.bootstrap_brokers_tls
+  value = compact(split(",", aws_msk_cluster.kafka.bootstrap_brokers_tls))
 }
