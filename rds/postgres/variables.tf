0e708f7ea7dd6461d798e31a4706ddb2c32fdafa
luktom <luktom@luktom.net>
2020-05-26 15:22:31 +0200

    Add modules

--- rds/postgres/variables.tf
+++ rds/postgres/variables.tf
@@ -1,4 +1,4 @@
-variable "name" { default = "db" }
+variable "name" { default = "database" }
 variable "apply_immediately" { default = false }
 variable "instance_class" { default = "db.t3.small" }
 variable "allocated_storage" { default = 20 }
