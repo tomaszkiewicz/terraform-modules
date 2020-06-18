ceee99f9554ea1cbf58305ea40e5dd6086f2f8ca
luktom <luktom@luktom.net>
2020-06-18 13:59:36 +0200

    Add modules

--- static-website/output.tf
+++ static-website/output.tf
@@ -1,9 +1,17 @@
-output "hosted_zone_id" {
+output "cloudfront_hosted_zone_id" {
   value = aws_cloudfront_distribution.website.hosted_zone_id
 }
 
-output "domain_name" {
+output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.website.domain_name
+}
+
+output "domain_name" {
+  value = var.domain_name
+}
+
+output "bucket_name" {
+  value = var.bucket_name
 }
 
 output "bucket_endpoint" {
