d4688a0a2bc7ac3d977f33341e27eec6033e4878
luktom <luktom@luktom.net>
2020-06-19 14:34:33 +0200

    Add modules

--- account/variables.tf
+++ account/variables.tf
@@ -2,5 +2,5 @@ variable "tenant" {}
 variable "environment" {}
 variable "alias_drop_master" { default = true }
 variable "aws_account_id" {}
-variable "aws_role_arn" {}
+variable "aws_role_arn" { default = "" }
 variable "aws_region" { default = "eu-west-1" }
