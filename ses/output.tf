d0f5c7ada24b848cc39f89ffd7354223be5704b8
luktom <luktom@luktom.net>
2020-06-26 13:21:01 +0200

    Add modules

--- ses/output.tf
+++ ses/output.tf
@@ -5,3 +5,7 @@ output "smtp_username" {
 output "smtp_password" {
   value = join("", aws_iam_access_key.ses.*.ses_smtp_password)
 }
+
+output "domain_identity_arns" {
+  value = zipmap(keys(var.domains), [ for x in aws_ses_domain_identity.domain: x.arn ])
+}
