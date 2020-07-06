a8e4f5a7d02d9c615146583af0c7582e0d41e104
luktom <luktom@luktom.net>
2020-07-06 12:08:01 +0200

    Add modules

--- alias/main.tf
+++ alias/main.tf
@@ -1,3 +1,3 @@
 resource "aws_iam_account_alias" "alias" {
-  account_alias = var.alias_drop_master && var.environment == "master" ? var.tenant : "${var.tenant}-${var.environment}"
+  account_alias = var.override_alias != "" ? var.override_alias : (var.alias_drop_master && var.environment == "master" ? var.tenant : "${var.tenant}-${var.environment}")
 }
--- alias/variables.tf
+++ alias/variables.tf
@@ -1,3 +1,4 @@
 variable "tenant" {}
 variable "environment" {}
 variable "alias_drop_master" { default = true }
+variable "override_alias" { default = "" }
