51bc4bb9df153c72d098a3d84b84c70fa181dbb5
luktom <luktom@luktom.net>
2020-08-14 16:32:50 +0200

    Add modules

--- cloudtrail/main.tf
+++ cloudtrail/main.tf
@@ -1,13 +1,3 @@
-variable "kms_key_id" {}
-variable "tenant" {}
-variable "is_multi_region_trail" { default = true }
-variable "is_organization_trail" { default = true }
-variable "s3_enable_transition" { default = true }
-variable "s3_days_to_transition" { default = 14 }
-variable "s3_transition_storage_class" { default = "GLACIER" }
-variable "s3_enable_expiration" { default = false }
-variable "s3_days_to_expiration" { default = 90 }
-
 locals {
   bucket = "${var.tenant}-cloudtrail"
 }
---
+++ cloudtrail/output.tf
@@ -0,0 +1,3 @@
+output "bucket" {
+  value = module.s3_bucket.bucket
+}
