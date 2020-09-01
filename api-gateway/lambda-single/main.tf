f11754c1e6fe4f3fb363349d66719dd45be7ebf7
luktom <luktom@luktom.net>
2020-09-01 16:48:11 +0200

    Add api gw integrations for root path

--- api-gateway/lambda-single/main.tf
+++ api-gateway/lambda-single/main.tf
@@ -1,3 +1,22 @@
+resource "aws_api_gateway_method" "root_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = "ANY"
+
+  authorization = var.authorization
+  authorizer_id = var.authorizer_id
+}
+
+resource "aws_api_gateway_integration" "root_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.lambda.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
 resource "aws_api_gateway_resource" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
