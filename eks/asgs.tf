11538ac3af6be62ad1c26144c8aaa6399c6b0c22
luktom <luktom@luktom.net>
2020-09-03 17:18:07 +0200

    Add large ASG for EKS

--- eks/asgs.tf
+++ eks/asgs.tf
@@ -30,6 +30,37 @@ locals {
     ]
   })
 
+  large_asg_launch_template = merge(var.override_ami_id != "" ? {
+    ami_id = var.override_ami_id
+  } : {}, {
+    name                    = "large"
+    override_instance_types = ["t3.large", "t2.large", "t3a.large"]
+    asg_max_size            = var.large_asg_max_size
+    asg_min_size            = var.large_asg_min_size
+    asg_desired_capacity    = var.large_asg_desired_capacity
+    on_demand_base_capacity = var.large_asg_on_demand_base_capacity
+    bootstrap_extra_args    = "--use-max-pods false"
+    #kubelet_extra_args      = "--node-labels=kubernetes.io/size=large"
+
+    tags = [
+      {
+        "key"                 = "k8s.io/cluster-autoscaler/enabled"
+        "propagate_at_launch" = "false"
+        "value"               = "true"
+      },
+      {
+        "key"                 = "k8s.io/cluster-autoscaler/${var.tenant_name}-${var.cluster_name}"
+        "propagate_at_launch" = "false"
+        "value"               = "true"
+      },
+      {
+        "key"                 = "k8s.io/cluster-autoscaler/node-template/label/type"
+        "propagate_at_launch" = "false"
+        "value"               = "large"
+      },
+    ]
+  })
+
   gitlab_ci_runner_medium_asg_launch_template = merge(var.override_ami_id != "" ? {
     ami_id = var.override_ami_id
   } : {}, {
@@ -68,6 +99,7 @@ locals {
 
   worker_groups_launch_template = [for x in concat([
     var.medium_asg_enabled ? local.medium_asg_launch_template : null,
+    var.large_asg_enabled ? local.large_asg_launch_template : null,
     var.gitlab_ci_runner_medium_asg_enabled ? local.gitlab_ci_runner_medium_asg_launch_template : null,
     ],
     var.additional_worker_groups_launch_templates
--- eks/variable.tf
+++ eks/variable.tf
@@ -15,6 +15,12 @@ variable "medium_asg_min_size" { default = 1 }
 variable "medium_asg_desired_capacity" { default = 1 }
 variable "medium_asg_on_demand_base_capacity" { default = 1 }
 
+variable "large_asg_enabled" { default = false }
+variable "large_asg_max_size" { default = 3 }
+variable "large_asg_min_size" { default = 1 }
+variable "large_asg_desired_capacity" { default = 1 }
+variable "large_asg_on_demand_base_capacity" { default = 1 }
+
 variable "gitlab_ci_runner_medium_asg_enabled" { default = false }
 variable "gitlab_ci_runner_medium_asg_max_size" { default = 3 }
 variable "gitlab_ci_runner_medium_asg_min_size" { default = 0 }
