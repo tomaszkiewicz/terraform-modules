285fa0a6700f11167a22cfef0e55d489d01fb1d6
luktom <luktom@luktom.net>
2020-09-24 17:24:56 +0200

    Add modules

--- slack-alarm-notification-lambda/main.tf
+++ slack-alarm-notification-lambda/main.tf
@@ -1,10 +1,11 @@
 module "lambda" {
   source = "../lambda"
 
-  name    = "slack-alarm-notification"
-  runtime = "go1.x"
-  handler = "main"
-  timeout = 10
+  name        = "slack-alarm-notification"
+  source_file = "/tmp/terraform-artifacts/lambda-slack-alarm-notification.zip"
+  runtime     = "go1.x"
+  handler     = "main"
+  timeout     = 10
 
   environment = {
     SLACK_WEBHOOK = var.webhook_url
