a9a6ad39f5f7af3c8907b17bc5acd9acf8749493
luktom <luktom@luktom.net>
2020-10-16 13:45:58 +0200

    Add modules

--- ses/variables.tf
+++ ses/variables.tf
@@ -1,5 +1,7 @@
 variable "bounce_notification_lambda_arn" {}
 variable "bounce_notification_lambda_name" {}
+variable "daily_sent_limit" { default = 0 }
+variable "notifications_sns_topic_arn" { default = "" }
 
 variable "domains" {
   description = "A map of domains with their zone id's"
