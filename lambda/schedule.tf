a0ce78498c5c29014df3d51dedce98f80539b1d3
luktom <luktom@luktom.net>
2020-10-20 16:29:09 +0200

    Add modules

--- lambda/schedule.tf
+++ lambda/schedule.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_event_rule" "schedule" {
 resource "aws_cloudwatch_event_target" "schedule" {
   count = var.schedule_expression != "" ? 1 : 0
 
-  rule = aws_cloudwatch_event_rule.schedule.name
+  rule = aws_cloudwatch_event_rule.schedule[0].name
   arn = join("",
     aws_lambda_function.lambda_external.*.arn,
     aws_lambda_function.lambda_source_dir.*.arn,
