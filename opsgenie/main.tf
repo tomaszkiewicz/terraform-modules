ade50443bdc88b509e3949d778d4a57ffbf66ccd
luktom <luktom@luktom.net>
2020-10-26 17:19:15 +0100

    Add modules

--- opsgenie/main.tf
+++ opsgenie/main.tf
@@ -3,7 +3,8 @@ resource "aws_sns_topic" "opsgenie_alarm_notification" {
 }
 
 resource "aws_sns_topic_subscription" "opsgenie_alarm_notification_subscription" {
-  topic_arn = aws_sns_topic.opsgenie_alarm_notification.arn
-  protocol  = "https"
-  endpoint  = "https://api.opsgenie.com/v1/json/cloudwatch?apiKey=${var.api_key}"
+  topic_arn              = aws_sns_topic.opsgenie_alarm_notification.arn
+  protocol               = "https"
+  endpoint_auto_confirms = true
+  endpoint               = "https://api.opsgenie.com/v1/json/cloudwatch?apiKey=${var.api_key}"
 }
