ceb841e9f31533ef51e1bf24273626ef5023f51a
luktom <luktom@luktom.net>
2020-10-30 12:24:13 +0100

    Add modules

--- sg/output.tf
+++ sg/output.tf
@@ -1,7 +1,7 @@
 output "security_group_id" {
-  value = "${aws_security_group.main.id}"
+  value = aws_security_group.main.id
 }
 
 output "id" {
-  value = "${aws_security_group.main.id}"
+  value = aws_security_group.main.id
 }
