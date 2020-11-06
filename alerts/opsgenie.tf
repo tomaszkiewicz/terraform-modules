8b9a41640c9cc17860c11385f2016d40d55ad91a
luktom <luktom@luktom.net>
2020-11-06 16:43:59 +0100

    Add modules

--- alerts/opsgenie.tf
+++ alerts/opsgenie.tf
@@ -4,5 +4,6 @@ module "opsgenie" {
   source = "./opsgenie"
   count  = var.opsgenie_api_key == "" ? 0 : 1
 
-  api_key = var.opsgenie_api_key
+  api_key       = var.opsgenie_api_key
+  sns_topic_arn = aws_sns_topic.alerts.arn
 }
--- alerts/slack.tf
+++ alerts/slack.tf
@@ -4,5 +4,6 @@ module "slack" {
   source = "./slack"
   count  = var.slack_webhook_url == "" ? 0 : 1
 
-  webhook_url = var.slack_webhook_url
+  webhook_url   = var.slack_webhook_url
+  sns_topic_arn = aws_sns_topic.alerts.arn
 }
