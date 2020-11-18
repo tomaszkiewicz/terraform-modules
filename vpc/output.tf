bfdb08d2d79ce4b58c8f3b1c6da43687882d879a
luktom <luktom@luktom.net>
2020-11-18 14:09:09 +0100

    Quick commit

--- vpc/output.tf
+++ vpc/output.tf
@@ -25,3 +25,11 @@ output "public_subnets" {
 output "public_subnet_ids" {
   value = module.vpc.public_subnets
 }
+
+output "public_subnets_ids_az" {
+  value = zipmap(data.aws_availability_zones.available.names, module.vpc.public_subnets)
+}
+
+output "private_subnets_ids_az" {
+  value = zipmap(data.aws_availability_zones.available.names, module.vpc.private_subnets)
+}
