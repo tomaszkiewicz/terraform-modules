400f0d7d0c6f361aca6dc6fc016127694ddd4ba3
Tomasz Nowodziński <tomasz.nowodzinski@gmail.com>
2020-12-03 15:20:11 +0100

    Added  OG EU region

--- alerts/opsgenie/main.tf
+++ alerts/opsgenie/main.tf
@@ -4,5 +4,5 @@ resource "aws_sns_topic_subscription" "opsgenie_alarm_notification_subscription"
   topic_arn              = var.sns_topic_arn
   protocol               = "https"
   endpoint_auto_confirms = true
-  endpoint               = "https://api.opsgenie.com/v1/json/cloudwatch?apiKey=${var.api_key}"
+  endpoint               = "https://api.%{if var.eu_region}eu.%{endif}opsgenie.com/v1/json/cloudwatch?apiKey=${var.api_key}"
 }
