b3c3d139d28227627ff3145950af628722b366f5
luktom <luktom@luktom.net>
2020-12-29 13:03:49 +0100

    Add modules

--- api-gateway-v2/ecs-service/output.tf
+++ api-gateway-v2/ecs-service/output.tf
@@ -1,3 +1,11 @@
+output "id" {
+  value = aws_apigatewayv2_api.main.id
+}
+
 output "api_id" {
   value = aws_apigatewayv2_api.main.id
+}
+
+output "default_integration_id" {
+  value = aws_apigatewayv2_intergration.default.id
 }
