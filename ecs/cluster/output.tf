0188ba4a551391bb418c472ec6338c97a71f95f0
luktom <luktom@luktom.net>
2021-01-03 22:16:09 +0100

    Add modules

--- ecs/cluster/output.tf
+++ ecs/cluster/output.tf
@@ -2,10 +2,18 @@ output "cluster_id" {
   value = module.ecs.this_ecs_cluster_id
 }
 
+output "cluster_name" {
+  value = var.cluster_name
+}
+
 output "namespace_id" {
   value = aws_service_discovery_private_dns_namespace.ecs.id
 }
 
 output "execution_role_arn" {
   value = module.iam_role_execution.arn
+}
+
+output "vpc_id" {
+  value = var.vpc_id
 }
