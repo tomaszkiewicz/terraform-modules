012b8452ca22da6a313c134104d0394003b7932c
luktom <luktom@luktom.net>
2021-01-27 16:58:16 +0100

    Add modules

--- ecs/cluster/main.tf
+++ ecs/cluster/main.tf
@@ -4,10 +4,12 @@ module "ecs" {
   name = var.cluster_name
 
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-  default_capacity_provider_strategy = {
-    capacity_provider = "FARGATE_SPOT"
-    weight            = 1
-  }
+  default_capacity_provider_strategy = [
+    {
+      capacity_provider = "FARGATE_SPOT"
+      weight            = 1
+    }
+  ]
 
   tags = {
     Name = var.cluster_name
