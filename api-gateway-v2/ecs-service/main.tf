963b9de22a4fd58caae7130a146493c4eaf5e9ae
Tomasz Nowodziński <tomasz.nowodzinski@gmail.com>
2021-01-29 16:23:22 +0100

    Added CORS configuration

--- api-gateway-v2/ecs-service/main.tf
+++ api-gateway-v2/ecs-service/main.tf
@@ -1,6 +1,19 @@
 resource "aws_apigatewayv2_api" "main" {
   name          = var.name
   protocol_type = "HTTP"
+
+  dynamic "cors_configuration" {
+    for_each = var.enable_cors ? ["hack"] : []
+
+    content {
+      allow_headers  = var.allow_headers
+      allow_methods  = var.allow_methods
+      allow_origins  = var.allow_origins
+      max_age  = var.max_age
+      allow_credentials = var.allow_credentials
+      expose_headers  = var.expose_headers
+    }
+  }
 }
 
 resource "aws_apigatewayv2_integration" "default" {
