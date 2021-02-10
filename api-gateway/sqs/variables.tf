4ada63d4cd535657a2296c0e0c7c2bc4bad81925
Dominik Kroliczek <dominik@kroliczek.com>
2021-02-10 17:06:08 +0100

    Add sqs validator option

--- api-gateway/sqs/variables.tf
+++ api-gateway/sqs/variables.tf
@@ -4,3 +4,10 @@ variable "path" {}
 variable "sqs_arn" {}
 variable "sqs_queue_name" {}
 variable "region" {}
+variable "request_validator_id" {
+  default = null
+}
+variable "request_models" {
+  type    = map
+  default = null
+}
