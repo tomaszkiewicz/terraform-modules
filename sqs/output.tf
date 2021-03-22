1aa1b2e528988b030d73db485a61db2b102cb936
Dominik Króliczek <kroliczeek1@gmail.com>
2021-03-22 15:47:00 +0100

    Add sqs module outputs (#10)

--- sqs/output.tf
+++ sqs/output.tf
@@ -6,6 +6,18 @@ output "name" {
   value = var.name
 }
 
+output "id" {
+  value = aws_sqs_queue.queue.id
+}
+
 output "dead_letter_arn" {
   value = aws_sqs_queue.dead_letter.arn
+}
+
+output "dead_letter_name" {
+  value = aws_sqs_queue.dead_letter.name
+}
+
+output "dead_letter_id" {
+  value = aws_sqs_queue.dead_letter.id
 }
