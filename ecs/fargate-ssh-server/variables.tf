681f4733709e8c89bc1654868b8fc195bbc960e5
alex <pukocz.aleksander@gmail.com>
2021-04-09 16:21:45 +0200

    fix fargate-ssh-server securitygroup cidr - cidr list str

--- ecs/fargate-ssh-server/variables.tf
+++ ecs/fargate-ssh-server/variables.tf
@@ -36,7 +36,7 @@ variable "notifications_sns_topic_arn" { default = "" }
 
 variable "service_discovery_namespace_id" { default = "" }
 
-variable "cidr_blocks" {
+variable "cidr_block" {
   type    = list(string)
   default = ["0.0.0.0/0"]
 }
