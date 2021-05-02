97483a1b2ed76b4626cd1dda770c9b21b51b8975
luktom <luktom@luktom.net>
2021-05-02 17:36:54 +0200

    Add modules

--- ecs/cluster/output.tf
+++ ecs/cluster/output.tf
@@ -1,17 +1,17 @@
 output "cluster_id" {
-  value = module.ecs.this_ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
 
 output "id" {
-  value = module.ecs.this_ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
 
 output "cluster_arn" {
-  value = module.ecs.this_ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
 
 output "arn" {
-  value = module.ecs.this_ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
 
 output "cluster_name" {
