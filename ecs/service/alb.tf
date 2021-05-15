f13fb14dd90797ec4ed3f8d3917d3f7f9740a0ca
luktom <luktom@luktom.net>
2021-05-15 17:12:57 +0200

    Add modules

--- ecs/service/alb.tf
+++ ecs/service/alb.tf
@@ -1,7 +1,7 @@
 resource "aws_lb_target_group" "ecs" {
   count = var.lb_listener_arn != "" ? 1 : 0
 
-  name                 = "${var.cluster_id}-service-${var.name}"
+  name                 = "${var.cluster_id}-${var.name}"
   port                 = var.service_port
   vpc_id               = var.vpc_id
   protocol             = "HTTP"
