75c5ed8860315c6514dc1880eea109db99b1a942
Lukasz Dronka <lukasz.dronka@pragmaticcoders.com>
2021-05-17 11:43:42 +0200

    Add possibility of enabling API Gateway access logs

--- api-gateway-v2/ecs-service/variables.tf
+++ api-gateway-v2/ecs-service/variables.tf
@@ -64,3 +64,7 @@ variable "cors_expose_headers" {
   type        = list(string)
   default     = []
 }
+
+variable "enable_access_log" { default = false }
+variable "access_log_format" { default = "{ \"requestId\":\"$context.requestId\", \"ip\": \"$context.identity.sourceIp\", \"httpMethod\":\"$context.httpMethod\",\"status\":\"$context.status\",\"path\": \"$context.path\" }" }
+variable "logs_retention_days" { default = 7 }
