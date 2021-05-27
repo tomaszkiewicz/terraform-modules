87fead2a7309653a40d1216a5971f0e949078ccc
kkosowsk <kamil.kosowski@pragmaticcoders.com>
2021-05-27 22:07:36 +0200

    add vars

--- ecs-alb/service/alb_private.tf
+++ ecs-alb/service/alb_private.tf
@@ -2,6 +2,7 @@ variable "alb_name" {}
 variable "alb_sg" {}
 variable "alb_subnets" {}
 variable "certificate_arn" {}
+variable "deregistration_delay" {default = 2}
 
 resource "aws_lb" "private_alb" {
   name               = var.alb_name
@@ -14,16 +15,16 @@ resource "aws_lb" "private_alb" {
 }
 
 resource "aws_lb_target_group" "default" {
-  name = "alb-tg-${var.image_name}"
-  port = 80
+  name = "target-group-${var.image_name}"
+  port = var.service_port
   protocol = "HTTP"
   vpc_id = "${var.vpc_id}"
   target_type = "ip"
-  deregistration_delay = 10
+  deregistration_delay = var.deregistration_delay
   health_check {
     healthy_threshold = "2"
     interval = "16"
-    path = "/health"
+    path = var.health_check_path
     timeout = "15"
     unhealthy_threshold = "2"
     matcher = "200-399"
