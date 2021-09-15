179fb8a976a76b69f9580c7a767678ece9fc6772
Maciej Demkiewicz <maciej.demkiewicz@pragmaticcoders.com>
2021-09-15 12:10:39 +0200

    [SRE-256]: Adding default CAA records for domains

--- dns/variables.tf
+++ dns/variables.tf
@@ -7,6 +7,15 @@ variable "delegations" {
 
 variable "caa_records" {
   type = list
-  default = []
+  default = [
+    "0 issue \"amazon.com\"",
+    "0 issue \"amazonaws.com\"",
+    "0 issue \"amazontrust.com\"",
+    "0 issue \"awstrust.com\"",
+    "0 issuewild \"amazon.com\"",
+    "0 issuewild \"amazonaws.com\"",
+    "0 issuewild \"amazontrust.com\"",
+    "0 issuewild \"awstrust.com\"",
+  ]
 }
 
