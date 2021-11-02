3938bf57bc1c573733a0f2f8e8ecf17465ec4b70
Bartlomiej Wodecki <bartlomiej.wodecki@pragmaticcoders.com>
2021-11-02 11:55:31 +0100

    add product prefix

--- lambda/alerts/variable.tf
+++ lambda/alerts/variable.tf
@@ -1,3 +1,4 @@
 variable "concurrent_executions_threshold" { default = 800 }
 variable "invocations_threshold" { default = 3600 }
 variable "notifications_sns_topic_arn" { default = "" }
+variable "product_prefix" { default = "" }
