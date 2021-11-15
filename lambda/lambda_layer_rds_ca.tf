8f4dc3c0016850486d6bcd8dbe419b376c12b372
Bartlomiej Wodecki <bartlomiej.wodecki@pragmaticcoders.com>
2021-11-15 16:16:49 +0100

    add layer name

--- lambda/lambda_layer_rds_ca.tf
+++ lambda/lambda_layer_rds_ca.tf
@@ -1,4 +1,5 @@
 module "lambda_layer_rds_ca" {
   count  = var.enable_lambda_layers_rds_ca != "" ? 1 : 0
-  source = "layers/rds-ca"
+  source = "./layers/rds-ca"
+  layer_name = var.lambda_layer_name
 }
--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,4 +1,5 @@
 variable "name" {}
+variable "lambda_layer_name" { default = "lambda_layer_rds_ca" }
 variable "runtime" { default = "nodejs12.x" }
 variable "handler" { default = "index.handler" }
 variable "source_dir" { default = "" }
