c512908bfa5af90d37aa9766a75a10cc93eade66
Bartlomiej Wodecki <bartlomiej.wodecki@pragmaticcoders.com>
2021-11-16 09:18:12 +0100

    fix review

--- lambda/locals.tf
+++ lambda/locals.tf
@@ -4,6 +4,6 @@ locals {
     var.enable_lambda_insights ? [
       "arn:aws:lambda:${data.aws_region.current.name}:580247275435:layer:LambdaInsightsExtension:${var.lambda_insights_version}",
     ] : [],
-    var.enable_lambda_layers_rds_ca ? [ module.lambda_layer_rds_ca.arn ] : []
+    var.enable_lambda_layers_rds_ca ? [ module.lambda_layer_rds_ca[0].arn ] : []
   )
 }
