62580dd17e97051bf3009831687d91c3b5372432
Bartlomiej Wodecki <bartlomiej.wodecki@pragmaticcoders.com>
2021-11-24 09:15:12 +0100

    add configuration set for ses

---
+++ ses/configuration_set.tf
@@ -0,0 +1,28 @@
+resource "aws_ses_configuration_set" "configuration_set" {
+  name  = "complaint-set"
+  reputation_metrics_enabled = true
+}
+
+resource "aws_ses_event_destination" "cloudwatch" {
+  name                   = "event-complaint-destination-cloudwatch"
+  configuration_set_name = aws_ses_configuration_set.configuration_set.name
+  enabled                = true
+  matching_types         = ["complaint", "bounce", "renderingFailure"]
+
+  cloudwatch_destination {
+    default_value  = "reputation"
+    dimension_name = "Email"
+    value_source   = "messageTag"
+  }
+}
+
+resource "aws_ses_event_destination" "sns" {
+  name                   = "event-complaint-destination-sns"
+  configuration_set_name = aws_ses_configuration_set.configuration_set.name
+  enabled                = true
+  matching_types         = ["complaint", "bounce", "renderingFailure"]
+
+  sns_destination {
+    topic_arn = aws_sns_topic.bounce_notification.arn
+  }
+}
