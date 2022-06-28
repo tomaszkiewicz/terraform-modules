dbaf2fcd914ec0a647fe06572256f354eb0fa31b
Arkadiusz Tomala <arkadiusz.tomala@pragmaticcoders.com>
2022-06-28 23:00:04 +0200

    change url

--- scripts/check-efs/timescaledb-live.sh
+++ scripts/check-efs/timescaledb-live.sh
@@ -6,6 +6,6 @@ if [ -f "$FILE" ]; then
     echo "file exists - OK"
 else
     cp /var/log/messages /s3bucketlog/qodex-timescaledb-live/messages"$(date -u)"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"EFS-qodex-timescaledb-live-TIMEOUT!!!"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"EFS-qodex-timescaledb-live-TIMEOUT!!!"}' https://hooks.slack.com/services/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
     echo "file send - OK"
 fi
