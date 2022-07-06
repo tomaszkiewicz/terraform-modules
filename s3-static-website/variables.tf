a7875b3c7c077f108be500632aa50e955c43c012
djpragmaticcoders <88495925+djpragmaticcoders@users.noreply.github.com>
2022-07-06 13:43:25 +0200

    Create variables.tf

---
+++ s3-static-website/variables.tf
@@ -0,0 +1,4 @@
+variable "versioning" { default = false }
+variable "bucket_name" { default = "" }
+variable "index_document" { default = "index.html" }
+variable "error_document" { default = "error404/index.html" }
