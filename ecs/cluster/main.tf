49d124e6ba8cf6375fcbc30d460e294b43cf60a9
luktom <luktom@luktom.net>
2023-01-28 23:19:24 +0100

    Update modules

--- ecs/cluster/main.tf
+++ ecs/cluster/main.tf
@@ -1,15 +1,15 @@
 module "ecs" {
   source = "terraform-aws-modules/ecs/aws"
 
-  name = var.cluster_name
+  cluster_name                          = var.cluster_name
 
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-  default_capacity_provider_strategy = [
-    {
-      capacity_provider = "FARGATE_SPOT"
-      weight            = 1
+  fargate_capacity_providers = {
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 1
+      }
     }
-  ]
+  }
 
   tags = {
     Name = var.cluster_name
