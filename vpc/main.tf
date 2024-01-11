910bd62c9df6b5a32bce0eeb3e6b9299596f1eee
Łukasz Tomaszkiewicz <luktom@luktom.net>
2024-01-11 18:32:13 +0100

    Adjust some VPC settings to new version

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,6 +4,7 @@ locals {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version  = "5.5.0"
 
   name = var.name
   cidr = var.cidr_block
@@ -32,12 +33,12 @@ module "vpc" {
   create_database_internet_gateway_route = true
 
   enable_ipv6                                    = var.enable_ipv6
-  assign_ipv6_address_on_creation                = var.enable_ipv6
   private_subnet_assign_ipv6_address_on_creation = var.enable_ipv6
 
   public_subnet_ipv6_prefixes   = [129, 130, 131]
   private_subnet_ipv6_prefixes  = [1, 2, 3]
-  database_subnet_ipv6_prefixes = [65, 66, 67]
+  database_subnet_ipv6_prefixes = []
+  #database_subnet_ipv6_prefixes = slice([65, 66, 67, 68, 60], 0, length(local.sliced_azs))
 
   enable_nat_gateway     = var.enable_nat_gateway
   single_nat_gateway     = var.production_mode == false
